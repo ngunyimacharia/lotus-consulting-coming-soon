e5aa73e2d8073c596a900b8c6c0dc584677a041a
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-06-05 17:17:25 +0200

    Name and favicon update

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'coming-soon',
+    title: 'Lotus Consulting -  Targeted Insights | Meaningful Impact ',
     htmlAttrs: {
       lang: 'en'
     },
